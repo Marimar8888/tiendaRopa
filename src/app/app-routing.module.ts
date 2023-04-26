27f71d2e7b14ee2f40146e4676d4b2368f3c2c83
Marimar <malonso@quadram.mobi>
2023-04-26 20:57:42 +0200

    apariencia sin terminar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './dashboard/home/home.component';
+import { TiendaComponent } from './dashboard/tienda/tienda.component';
+import { NosotrosComponent } from './dashboard/nosotros/nosotros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'tienda', component: TiendaComponent},
+  {path: 'nosotros', component: NosotrosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './dashboard/home/home.component';
+import { TiendaComponent } from './dashboard/tienda/tienda.component';
+import { NosotrosComponent } from './dashboard/nosotros/nosotros.component';
+import { NavComponent } from './layout/nav/nav.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { CarrouselComponent } from './layout/carrousel/carrousel.component';
+import { BannersComponent } from './layout/banners/banners.component';
+import { CategoriasComponent } from './layout/categorias/categorias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TiendaComponent,
+    NosotrosComponent,
+    NavComponent,
+    HeaderComponent,
+    CarrouselComponent,
+    BannersComponent,
+    CategoriasComponent
   ],
   imports: [
     BrowserModule,
