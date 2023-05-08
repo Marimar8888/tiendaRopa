0501708477fa1ab5a58b5e097595721424b81bd0
Marimar <malonso@quadram.mobi>
2023-05-08 12:39:27 +0200

    apariencia ficha y recepcion datos acabado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './dashboard/home/home.component';
 import { TiendaComponent } from './dashboard/tienda/tienda.component';
 import { NosotrosComponent } from './dashboard/nosotros/nosotros.component';
-import { ArticuloFormComponent } from './entidades/articulo/articulo-form/articulo-form.component';
 import { AbrigosComponent } from './dashboard/tienda/abrigos/abrigos.component';
 import { ChaquetasComponent } from './dashboard/tienda/chaquetas/chaquetas.component';
 import { PantalonesComponent } from './dashboard/tienda/pantalones/pantalones.component';
 import { SportsComponent } from './dashboard/tienda/sports/sports.component';
 import { CorbatasComponent } from './dashboard/tienda/corbatas/corbatas.component';
 import { FichaComponent } from './dashboard/ficha/ficha.component';
+
 
 const routes: Routes = [
   {path: '', component: HomeComponent },
@@ -20,7 +20,7 @@ const routes: Routes = [
   {path: 'tienda/chaquetas', component: ChaquetasComponent },
   {path: 'tienda/pantalones', component: PantalonesComponent },
   {path: 'tienda/sports', component: SportsComponent },
-  {path: 'tienda/corbatas', component: CorbatasComponent }
+  {path: 'tienda/corbatas', component: CorbatasComponent },
 
 ];
 
--- src/app/entidades/articulo/articulo-form/articulo-form.component.ts
+++ src/app/entidades/articulo/articulo-form/articulo-form.component.ts
@@ -1,6 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { Articulo } from '../model/articulo.model';
+
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+registerLocaleData(localeEs, 'es');
 
 @Component({
   selector: 'app-articulo-form',
